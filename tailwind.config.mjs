9b567edb7897f339df5b45cedb279952fa69724b
Ale Aguiar Díaz <122371223+aleaguiard@users.noreply.github.com>
2024-08-16 18:10:36 +0100

    feat(layout): Adjust image layout for mobile screens

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "class",
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				wave: 'wave 0.6s ease-in-out infinite',
+			},
+			keyframes: {
+				wave: {
+					'0%': { transform: 'rotate(0deg)' },
+					'20%': { transform: 'rotate(15deg)' },
+					'40%': { transform: 'rotate(0deg)' },
+					'60%': { transform: 'rotate(-15deg)' },
+					'80%': { transform: 'rotate(0deg)' },
+					'100%': { transform: 'rotate(0deg)' },
+				},
+			},
+		},
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [require('@tailwindcss/typography')],
 };
